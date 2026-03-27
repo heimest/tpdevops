338d152b0298796ca9d8f6140b360c5b3ce8a84f
alexandreborny <spamalbo@icloud.com>
2026-03-27 15:28:40 +0100

    feat: added a test fix: moved HelloController to controller folder

--- src/main/java/com/example/tpdevops/TpdevopsApplication.java
+++ src/main/java/com/example/tpdevops/TpdevopsApplication.java
@@ -2,20 +2,12 @@ package com.example.tpdevops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TpdevopsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TpdevopsApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello from DockerHub!";
     }
 
 }
